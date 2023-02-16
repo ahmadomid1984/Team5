a243498fa7699697311f3ff52e12c52907d87db0
ahmadomid1984 <111872990+ahmadomid1984@users.noreply.github.com>
2023-02-16 20:28:44 +0200

    main page with header and footer are created

---
+++ footer.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sansita&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Damion&display=swap" rel="stylesheet">
+    <title>Crush Coffee</title>
+</head>
+<div class="bg-image"></div>
+
+<footer>
+    <div class="grid-container2">
+        <div>
+            <p class="first">ABOUT US</p>
+            <p>Our Company</p>
+            <p>Privacy & Policy</p>
+            <p>ADA Compliance</p>
+        </div>
+        <div>
+            <p class="second">CONTACT US</p>
+            <p>041-450-0000</p>
+            <p>info@crushcoffee.com</p>
+            <p>Ahucatu 15, 13300 Hämeenlinna</p>
+        </div>
+        <div>
+            <p class="third">FIND US ON</p>
+            <p>Facebook</p>
+            <p>Instagram</p>
+            <p>Twitter & YouTube</p>        
+        </div>
+    </div>
+  </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sansita&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Damion&display=swap" rel="stylesheet">
+    <title>Crush Coffee</title>
+</head>
+<body>
+    <div class="bg-image"></div>
+
+    <header>
+        <ul class="topnav">
+            <li class="right"><a href="index.php">Crush Café</a></li>
+            <li><a class="active" href="index.php"><u>Home</u></a></li>
+            <li class="Coffee"><a href="#">Coffee</a></li>
+            <li class="Find"><a href="#">Find US</a></li>
+            <li class="About"><a href="#">About US</a></li>
+          </ul>          
+    </header>
+
+    <section class="sentence">
+                <div>
+                    <h2>We Take Coffee <br>
+                        Very Seriously</h2>
+                </div>
+    </section>
+      
