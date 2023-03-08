5583cf4e78346d2433c6c388d49d026a6a7bfe9a
ahmadomid1984 <111872990+ahmadomid1984@users.noreply.github.com>
2023-03-08 11:02:10 +0200

    index, header and footer

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include "./header.php" ?>
+
+<section class="grid-container">  
+        <div class="container20">
+            <a href="HotDrinks.php" style="text-decoration:none"><img src="img/left.jpg" alt="Hot" style="max-width:300px">    
+            <div class="content">
+                <h1>Hot Drinks</h1>
+            </div></a> 
+        </div>
+        <div class="container20">
+            <a href="ColdDrinks.php" style="text-decoration:none"> <img src="img/center.jpg" alt="Cold" style="max-width:300px">
+            <div class="content">
+                <h1>Cold Drinks</h1>
+            </div></a> 
+        </div>
+        <div class="container20">
+            <a href="Cakes.php" style="text-decoration:none"><img src="img/right.jpg" alt="Cakes" style="max-width:300px">
+            <div class="content">
+                <h1>Cakes</h1>
+            </div></a> 
+        </div>
+    </section>
+
+    <div class="text">
+        <p>"Welcome to Crush café, where every cup is crafted with care and passion. Our beans are sourced from the finest farms and roasted to perfection, ensuring a rich and satisfying taste in every sip  in our cozy atmosphere. Whether you're in need of a morning pick-me-up or an afternoon escape, our cozy and inviting atmosphere is the perfect place to relax and refuel. Stop by today and let us help you crush the day ahead!"</p>
+  </div>
+
+  <?php include "./footer.php" ?>
